29413a6e680fa7732fca2a928148cd653554677b
riccardocadei <riccardo.cadei98@gmail.com>
2025-06-13 16:35:24 +0200

    add fineturning script

--- scripts/main.sh
+++ scripts/main.sh
@@ -5,13 +5,13 @@
 #---------------------------------------------
 #
 #
-#SBATCH --job-name=ants_main             # Job name
-#SBATCH --output=./logs/ants_main%j.log  # Output file
-#SBATCH --time=04:00:00                  # Maximum walltime
-#SBATCH --ntasks=1                       # Number of tasks
-#SBATCH --mem=10G                        # Memory allocation
-#SBATCH --partition=gpu100              # Partition to use (e.g., gpu)
-#SBATCH --gres=gpu:1                    # Number of GPUs requested (adjust as needed)
+#SBATCH --job-name=ants_main              # Job name
+#SBATCH --output=./logs/ants_main_%j.log  # Output file
+#SBATCH --time=04:00:00                   # Maximum walltime
+#SBATCH --ntasks=1                        # Number of tasks
+#SBATCH --mem=10G                         # Memory allocation
+#SBATCH --partition=gpu100                # Partition to use (e.g., gpu)
+#SBATCH --gres=gpu:1                      # Number of GPUs requested (adjust as needed)
 #
 # Load your environment or module
 ##source ~/.bashrc                      # Ensure you load your bash profile
@@ -19,9 +19,8 @@ module load conda
 conda activate crl                      # Activate your conda environment
 
 # general variables 
-dir="./data/istant_lq"
-preprocessed="preprocessed/0.99"
-#preprocessed="original"
+data_dir="./data/v2"
+results_dir="./results/v2"
 
 # Run experiments
-srun python scripts/run_main.py --data_dir $dir --results_dir $dir --preprocessed $preprocessed
+srun python scripts/run_main.py --data_dir $data_dir --results_dir $results_dir
